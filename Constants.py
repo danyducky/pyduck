bc5090527e7e27f01aded4059795ae3e0e8baab8
danyducky <morikov2000@gmail.com>
2020-02-08 17:17:43 +0500

    NewProject

--- Constants.py
+++ Constants.py
@@ -1,3 +1,8 @@
-width = 1024
-height = 768
+width = 800
+height = 600
+
+start_x = 100
+start_y = 100
+
+background = 'data/background.jpg'
 
--- game.py
+++ game.py
@@ -1,6 +1,38 @@
 import pygame
 from Constants import *
 
-pygame.init()
 
+class Main():
+    """Основной класс"""
+    def __init__(self, screen):
+        self.screen = screen
+        self.run_game = True
+        self.x = start_x
+        self.y = start_y
+        self.background = pygame.image.load(background)
+        self.main_loop()
+
+    def render(self):
+        """Прорисовка всего-всего"""
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.flip()
+
+
+    def main_loop(self):
+        """Основной цикл игры"""
+        while self.run_game == True:
+            self.render() # пока идёт игра - будем прорисовывать все объекты из метода render
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run_game = False
+                if event.type == pygame.K_ESCAPE:
+                    self.run_game = False
+
+
+
+
+
+
+pygame.init()
 screen = pygame.display.set_mode((width, height))
+game = Main(screen)
