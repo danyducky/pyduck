d4eff6b9665c3b22333bec0891851d19ede47296
danyducky <morikov2000@gmail.com>
2020-02-08 15:31:45 +0500

    TestForVlad

--- ClassTest.py
+++ ClassTest.py
@@ -1,11 +1,11 @@
-# Master Branch
+087 # Master our Branch
 
 A = 10
-B = 20
-
-print(A,B)
+B = 90
+E = 285
+29384239842347329487print(A,B) # Was College Branch
 
 C = 20
-D = 30
-
-print(C,D)
+D = 30asddada sda
+dasdasd
+print(C,D) asdasdasd
