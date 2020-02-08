55719bf13eeaeea50d0f178fc97c49419f2a791b
danyducky <morikov2000@gmail.com>
2020-02-08 23:48:30 +0500

    player moving and animation

--- Constants.py
+++ Constants.py
@@ -1,14 +1,28 @@
 import pygame
-width = 700
-height = 600
+width = 900
+height = 400
 
 start_x = 100
-start_y = 100
-speed = 1
+start_y = 272
+speed = 3
+clock = pygame.time.Clock()
 
-background = pygame.image.load('data/background.jpg')
-player = pygame.image.load('data/player.png')
-player = pygame.transform.scale(player, (50, 80))
-playerL = pygame.image.load('data/playerL.png')
-playerL = pygame.transform.scale(playerL, (50, 80))
+background = pygame.image.load('data/background1.jpg')
+background = pygame.transform.scale(background, (width, height))
+player_img = pygame.image.load('data/player.png')
+player_img = pygame.transform.scale(player_img, (50, 80))
 
+
+pack_L = []
+packL = [pygame.image.load('data/playerL.png'), pygame.image.load('data/playerL1.png'),
+         pygame.image.load('data/playerL2.png'), pygame.image.load('data/playerL3.png')]
+for img in packL:
+    img = pygame.transform.scale(img, (50, 80))
+    pack_L.append(img)
+
+pack_R = []
+packR = [pygame.image.load('data/player.png'), pygame.image.load('data/player1.png'),
+         pygame.image.load('data/player2.png'), pygame.image.load('data/player3.png')]
+for imgR in packR:
+    imgR = pygame.transform.scale(imgR, (50, 80))
+    pack_R.append(imgR)
