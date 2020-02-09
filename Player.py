32a55ab4b9ab1d8c6735db1576c93abf4995f749
danyducky <morikov2000@gmail.com>
2020-02-09 12:42:43 +0500

    NewMovement2

--- Player.py
+++ Player.py
@@ -2,12 +2,12 @@ import pygame
 from Constants import *
 
 class player():
-    def __init__(self):
+    def __init__(self, name):
         self.x = start_x
         self.y = start_y
+        self.name = name
         self.leftImage = pack_L
         self.rightImage = pack_R
-        self.afkImage = player_img
         self.counter_L = 0
         self.counter_R = 0
 
@@ -15,21 +15,26 @@ class player():
     def move(self, screen):
         """Перемещение влево/вправо и анимация движения персонажа"""
         """Ограничение выхода за пределы окна"""
+        mouse = pygame.mouse.get_pos()
         key = pygame.key.get_pressed()
-        if key[pygame.K_d] == 0 and key[pygame.K_a] == 0:
-            screen.blit(self.afkImage, (self.x, self.y))
-        if key[pygame.K_a] == 1:
-            self.x -= speed
-            self.counter_L += 1
-            screen.blit(self.leftImage[self.counter_L // 3], (self.x, self.y))
-            if self.counter_L == 9:
-                self.counter_L = 0
-        if key[pygame.K_d]:
-            self.x += speed
-            self.counter_R += 1
-            screen.blit(self.rightImage[self.counter_R // 3], (self.x, self.y))
-            if self.counter_R == 9:
-                self.counter_R = 0
+        if mouse[0] < self.x:
+            if key[pygame.K_a] == 0:
+                screen.blit(self.leftImage[0], (self.x, self.y))
+            elif key[pygame.K_a]:
+                self.x -= speed
+                self.counter_L += 1
+                screen.blit(self.leftImage[self.counter_L // 3], (self.x, self.y))
+                if self.counter_L == 9:
+                    self.counter_L = 0
+        if mouse[0] > self.x:
+            if key[pygame.K_d] == 0:
+                screen.blit(self.rightImage[0], (self.x, self.y))
+            elif key[pygame.K_d]:
+                self.x += speed
+                self.counter_R += 1
+                screen.blit(self.rightImage[self.counter_R // 3], (self.x, self.y))
+                if self.counter_R == 9:
+                    self.counter_R = 0
 
         if self.x <= 0:
             self.x = 0
@@ -39,4 +44,3 @@ class player():
             self.x = width - 50
         if self.y >= height - 80:
             self.y = height - 80
-
